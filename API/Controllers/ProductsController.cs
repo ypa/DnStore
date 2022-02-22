e3a571031d024ca628e50e95586d868b171fcb1a
Yan P Aung <yan.pye.aung@gmail.com>
2022-02-21 19:28:44 -0800

    Refactor methods as async tasks

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 
@@ -18,17 +20,15 @@ namespace API.Controllers
             this.context = context;
         }
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = context.Products.ToList();
-
-            return Ok(products);
+            return await context.Products.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Product> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return context.Products.Find(id);
+            return await context.Products.FindAsync(id);
         }
     }
 
