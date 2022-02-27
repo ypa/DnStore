6674e18ba5efd6205490610b3e091e3eb5046e1c
Yan P Aung <yan.pye.aung@gmail.com>
2022-02-26 19:52:18 -0800

    Refactor ProductList and ProductCard components

---
+++ client/src/features/catalog/ProductCard.tsx
@@ -0,0 +1,19 @@
+import { Avatar, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import { Product } from '../../app/models/product';
+
+interface Props {
+  product: Product;
+}
+
+export default function ProudctCard({ product }: Props) {
+  return (
+    <ListItem key={product.id}>
+      <ListItemAvatar>
+        <Avatar src={product.pictureUrl} />
+      </ListItemAvatar>
+      <ListItemText>
+        {product.name} - {product.price}
+      </ListItemText>
+    </ListItem>
+  );
+}
