68808f4748db103b5c3170f2cb689684d0d57da8
Yan P Aung <yan.pye.aung@gmail.com>
2022-03-13 19:43:28 -0700

    Setup entity relationship with basket and basket item

--- API/Entities/BasketItem.cs
+++ API/Entities/BasketItem.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities
 {
+    [Table("BasketItems")]
     public class BasketItem
     {
         public int Id { get; set; }
@@ -9,5 +12,9 @@ namespace API.Entities
         public int ProductId { get; set; }
 
         public Product Product { get; set; }
+
+        public int BasketId { get; set; }
+
+        public Basket Basket { get; set; }
     }
 }
