5cdd8b1b2f2c38a7ffc19ec34b7711599b07bfd3
Yan P Aung <yan.pye.aung@gmail.com>
2022-03-24 20:20:14 -0700

    Spike redux toolkit

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,10 +1,11 @@
 import { Button, ButtonGroup, Typography } from '@mui/material';
-import { useDispatch, useSelector } from 'react-redux';
-import { CounterState, decrement, increment } from './counterReducer';
+import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
+import { decrement, increment } from './counterSlice';
 
 export default function ContactPage() {
-  const dispatch = useDispatch();
-  const { data, title } = useSelector((state: CounterState) => state);
+  // const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
+  const { data, title } = useAppSelector((state) => state.counter);
 
   return (
     <>
@@ -12,14 +13,14 @@ export default function ContactPage() {
       <Typography variant="h5">The data is: {data}</Typography>
       <ButtonGroup>
         <Button
-          onClick={() => dispatch(decrement())}
+          onClick={() => dispatch(decrement(1))}
           variant="contained"
           color="error"
         >
           Decrement
         </Button>
         <Button
-          onClick={() => dispatch(increment())}
+          onClick={() => dispatch(increment(1))}
           variant="contained"
           color="primary"
         >
