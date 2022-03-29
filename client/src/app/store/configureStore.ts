61814b9dea8c25720dd05c319e2cff01af9930ec
Yan P Aung <yan.pye.aung@gmail.com>
2022-03-28 19:17:39 -0700

    Add catalog slice

--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { basketSlice } from "../../features/basket/basketSlice";
+import { catalogSlice } from "../../features/catalog/catalogSlice";
 // import { createStore } from "redux";
 // import counterReducer from "../../features/contact/counterReducer";
 import { counterSlice } from "../../features/contact/counterSlice";
@@ -13,6 +14,7 @@ export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
     basket: basketSlice.reducer,
+    catalog: catalogSlice.reducer,
   }
 })
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,8 +7,11 @@ import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { Provider } from 'react-redux';
 import { store } from './app/store/configureStore';
+import { fetchProductsAsync } from './features/catalog/catalogSlice';
 
 export const history = createBrowserHistory();
+
+store.dispatch(fetchProductsAsync());
 
 ReactDOM.render(
   <React.StrictMode>
