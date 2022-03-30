c84081762c7348b17de8c5476dd976243f04547e
Yan P Aung <yan.pye.aung@gmail.com>
2022-03-29 20:15:17 -0700

    Fix loading indicator when updating cart

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -125,7 +125,7 @@ export default function ProductDetails() {
               disabled={
                 item?.quantity === quantity || (!item && quantity === 0)
               }
-              loading={status.includes('pendingRemoveItem' + item?.productId)}
+              loading={status.includes('pending' + item?.productId)}
               onClick={handleUpdateCart}
               sx={{ height: '55px' }}
               color="primary"
