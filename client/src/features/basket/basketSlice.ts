f8eed1ea65c1a53c3592f5bf051bea94bdbe7bba
Yan P Aung <yan.pye.aung@gmail.com>
2022-03-30 19:30:22 -0700

    Handle error in basketSlice when adding and removing products

--- client/src/features/basket/basketSlice.ts
+++ client/src/features/basket/basketSlice.ts
@@ -14,22 +14,22 @@ const initialState: BasketState = {
 
 export const addBasketItemAsync = createAsyncThunk<Basket, { productId: number, quantity?: number }>(
   'basket/addBasketItemAsync',
-  async ({ productId, quantity = 1 }) => {
+  async ({ productId, quantity = 1 }, thunkAPI) => {
     try {
       return await agent.Basket.addItem(productId, quantity);
-    } catch (error) {
-      console.log(error)
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue({ error: error.data });
     }
   }
 )
 
 export const removeBasketItemAsync = createAsyncThunk<void, { productId: number, quantity: number, name?: string }>(
   'basket/removeBasketItemAsync',
-  async ({ productId, quantity }) => {
+  async ({ productId, quantity }, thunkAPI) => {
     try {
       await agent.Basket.removeItem(productId, quantity);
-    } catch (error) {
-      console.log(error);
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue({ error: error.data });
     }
   }
 )
@@ -52,8 +52,9 @@ export const basketSlice = createSlice({
       state.status = 'idle';
     });
 
-    builder.addCase(addBasketItemAsync.rejected, (state) => {
+    builder.addCase(addBasketItemAsync.rejected, (state, action) => {
       state.status = 'idle';
+      console.log(action.payload);
     });
 
     builder.addCase(removeBasketItemAsync.pending, (state, action) => {
@@ -70,8 +71,9 @@ export const basketSlice = createSlice({
       state.status = 'idle';
     });
 
-    builder.addCase(removeBasketItemAsync.rejected, (state) => {
+    builder.addCase(removeBasketItemAsync.rejected, (state, action) => {
       state.status = 'idle';
+      console.log(action.payload);
     });
   })
 })
