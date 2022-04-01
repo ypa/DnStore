a801ff124c60a26acc70992c4a3a5e475a9e84cb
Yan P Aung <yan.pye.aung@gmail.com>
2022-03-31 19:51:31 -0700

    Add types and brands filters

--- API/Extensions/ProductExtensions.cs
+++ API/Extensions/ProductExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Entities;
+using Microsoft.Extensions.Logging.Configuration;
 
 namespace API.Extensions
 {
@@ -29,6 +30,23 @@ namespace API.Extensions
             var lowerCaseSearchTerm = searchTerm.Trim().ToLower();
 
             return query.Where(p => p.Name.ToLower().Contains(lowerCaseSearchTerm));
+        }
+
+        public static IQueryable<Product> Filter(this IQueryable<Product> query, string brands, string types)
+        {
+            var brandList = new List<string>();
+            var typeList = new List<string>();
+
+            if (!string.IsNullOrEmpty(brands))
+                brandList.AddRange(brands.ToLower().Split(",").ToList());
+
+            if (!string.IsNullOrEmpty(types))
+                typeList.AddRange(types.ToLower().Split(",").ToList());
+
+            query = query.Where(p => brandList.Count == 0 || brandList.Contains(p.Brand.ToLower()));
+            query = query.Where(p => typeList.Count == 0 || typeList.Contains(p.Type.ToLower()));
+
+            return query;
         }
     }
 }
