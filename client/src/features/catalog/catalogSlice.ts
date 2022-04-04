330a93808a000e9431351e92266452c6e97f53f5
Yan P Aung <yan.pye.aung@gmail.com>
2022-04-03 20:10:16 -0700

    Get filter lists from API and store them in Redux

--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -28,11 +28,25 @@ export const fetchProductAsync = createAsyncThunk<Product, number>(
   }
 )
 
+export const fetchFilters = createAsyncThunk(
+  'catalog/fetchFilters',
+  async (_, thunkAPI) => {
+    try {
+      return agent.Catalog.fetchFilters();
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue({ error: error.data });
+    }
+  }
+)
+
 export const catalogSlice = createSlice({
   name: 'catalog',
   initialState: productsAdapter.getInitialState({
     productsLoaded: false,
-    status: 'idle'
+    filtersLoaded: false,
+    status: 'idle',
+    brands: [],
+    types: []
   }),
   reducers: {},
   extraReducers: (builder => {
@@ -59,6 +73,19 @@ export const catalogSlice = createSlice({
       console.log(action);
       state.status = 'idle';
     });
+    builder.addCase(fetchFilters.pending, (state) => {
+      state.status = 'pendingFetchFilters';
+    });
+    builder.addCase(fetchFilters.fulfilled, (state, action) => {
+      state.brands = action.payload.brands;
+      state.types = action.payload.types;
+      state.filtersLoaded = true;
+      state.status = 'idle';
+    });
+    builder.addCase(fetchFilters.rejected, (state, action) => {
+      state.status = 'idle';
+      console.log(action.payload);
+    })
   })
 })
 
