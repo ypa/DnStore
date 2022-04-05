a9ff05f9c8939e5473d082c53e3c44a3cfc351de
Yan P Aung <yan.pye.aung@gmail.com>
2022-04-04 19:50:36 -0700

    Add product prams to redux

--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -1,7 +1,17 @@
 import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
+import { boolean } from "yup";
 import agent from "../../app/api/agent";
-import { Product } from "../../app/models/product";
+import { Product, ProductParams } from "../../app/models/product";
 import { RootState } from "../../app/store/configureStore";
+
+interface CatalogState {
+  productsLoaded: boolean;
+  filtersLoaded: boolean;
+  status: string;
+  brands: string[];
+  types: string[];
+  productParams: ProductParams;
+}
 
 const productsAdapter = createEntityAdapter<Product>();
 
@@ -39,16 +49,33 @@ export const fetchFilters = createAsyncThunk(
   }
 )
 
+function initParams() {
+  return {
+    pageNumber: 1,
+    pageSize: 6,
+    orderBy: 'name'
+  }
+}
+
 export const catalogSlice = createSlice({
   name: 'catalog',
-  initialState: productsAdapter.getInitialState({
+  initialState: productsAdapter.getInitialState<CatalogState>({
     productsLoaded: false,
     filtersLoaded: false,
     status: 'idle',
     brands: [],
-    types: []
+    types: [],
+    productParams: initParams()
   }),
-  reducers: {},
+  reducers: {
+    setProductParams: (state, action) => {
+      state.productsLoaded = false;
+      state.productParams = { ...state.productParams, ...action.payload }
+    },
+    resetProductParams: (state) => {
+      state.productParams = initParams();
+    }
+  },
   extraReducers: (builder => {
     builder.addCase(fetchProductsAsync.pending, (state) => {
       state.status = 'pendingFetchProducts';
@@ -90,3 +117,5 @@ export const catalogSlice = createSlice({
 })
 
 export const productSelectors = productsAdapter.getSelectors((state: RootState) => state.catalog);
+
+export const { setProductParams, resetProductParams } = catalogSlice.actions;
