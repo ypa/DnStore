6bd37c36e8c944a376655abc5234621ab5fee878
Yan P Aung <yan.pye.aung@gmail.com>
2022-05-02 19:58:21 -0700

    Implement basket transfer client side

--- client/src/features/basket/basketSlice.ts
+++ client/src/features/basket/basketSlice.ts
@@ -57,6 +57,9 @@ export const basketSlice = createSlice({
   reducers: {
     setBasket: (state, action) => {
       state.basket = action.payload
+    },
+    clearBasket: (state) => {
+      state.basket = null;
     }
   },
   extraReducers: (builder => {
@@ -96,4 +99,4 @@ export const basketSlice = createSlice({
   })
 })
 
-export const { setBasket } = basketSlice.actions;
+export const { setBasket, clearBasket } = basketSlice.actions;
