92895ef800f6c8e931327d09d2bfa6be20eb299d
Yan P Aung <yan.pye.aung@gmail.com>
2022-05-04 19:56:53 -0700

    Add order entities

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Entities;
+using API.Entities.OrderAggregate;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -18,6 +19,8 @@ namespace API.Data
         public DbSet<Product> Products { get; set; }
 
         public DbSet<Basket> Baskets { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
