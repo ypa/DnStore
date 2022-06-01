fc875d96365862118c725f3bfbb72a31e95c545d
Yan P Aung <yan.pye.aung@gmail.com>
2022-05-31 19:45:34 -0700

    add migrations for added properties of PyamentIntentId and ClientSecret:
    $ dotnet ef migrations add PaymentIntentAdded

---
+++ API/Data/Migrations/20220601024422_PaymentIntentAdded.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class PaymentIntentAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Orders",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientSecret",
+                table: "Baskets",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Baskets",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "cb49eadd-00fa-4cee-8e87-e77e3a63307e");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "ee80c3dd-020f-47bd-a60d-1f13276ee32e");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ClientSecret",
+                table: "Baskets");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Baskets");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "fa80ae8c-b1ee-4e66-89f7-9c0d38e2ccc6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "77824a81-7ac9-4565-9cd0-c0af85969fdb");
+        }
+    }
+}
