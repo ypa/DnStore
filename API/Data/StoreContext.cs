3e8fbb50cde597329d214f014a4f14e928b31e95
Yan P Aung <yan.pye.aung@gmail.com>
2022-06-11 20:05:09 -0700

    Fix datetime stamp compatibility

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -14,6 +14,7 @@ namespace API.Data
     {
         public StoreContext(DbContextOptions options) : base(options)
         {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
         }
 
         public DbSet<Product> Products { get; set; }
