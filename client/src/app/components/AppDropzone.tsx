107e9bc1e6364878ca85633d9a9e53285723bb9f
Yan P Aung <yan.pye.aung@gmail.com>
2022-06-20 09:15:42 -0700

    Add validation to the product form

--- client/src/app/components/AppDropzone.tsx
+++ client/src/app/components/AppDropzone.tsx
@@ -39,6 +39,7 @@ export default function AppDropzone(props: Props) {
     <div {...getRootProps()}>
       <FormControl
         style={isDragActive ? { ...dzStyles, ...dzActive } : dzStyles}
+        error={!!fieldState.error}
       >
         <input {...getInputProps()} />
         <UploadFile sx={{ fontSize: '100px' }} />
